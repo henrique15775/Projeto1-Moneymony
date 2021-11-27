dd7f697b3d4f5892f0da0f6eec21abefcde45db8
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2021-11-27 02:52:52 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+//import { LoginUsuarioComponent } from './user-workspace/login-usuario/login-usuario.component';
+import { ViewCotacaoComponent } from './user-workspace/view-cotacao/view-cotacao.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'viewcotacao',
+  component: ViewCotacaoComponent
+},
+  {path:'',
+  component: ViewCotacaoComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
